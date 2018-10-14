39e629c28b9685354d10ecc5462e5a73161ed1f1
Rhian Taylor <rhiant413@gmail.com>
2018-10-14 13:18:55 +0100

    python practise loops and such

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hello World \n")
+
+myList = ["CV","Learn to code", "Aplly for grad jobs", "figure out what to do with my life"]
+
+for item in myList:
+    print(item)
+
+name = input("what is your name? \n")
+print("your name is " + name)
