b228319bc3a7cacf2686ec47b1aa1bdd310c5710
Rhian Taylor <rhiant413@gmail.com>
2018-11-13 09:27:38 +0000

    uable to import textract to handle the PDF extraction

--- scraper.py
+++ scraper.py
@@ -2,6 +2,19 @@
 import importlib
 import urllib
 from bs4 import BeautifulSoup
+import PyPDF2
+
+#import pdfminer
+
+def fixPDF(a):
+    #source : https://mail.python.org/pipermail/python-list/2010-November/592460.html
+    try:
+        fileOpen = open(a, "a")
+        fileOpen.writer("%%EOF")
+        fileOpen.close
+        return "fixed"
+    except Exception:
+        return "unable to open"
 
 #specify URL
 fda_website = "https://www.accessdata.fda.gov/scripts/cder/ob/index.cfm"
@@ -14,5 +27,32 @@ soup = BeautifulSoup(page_html,'html.parser')
 
 #print(soup.prettify())
 
-input1 = soup.find(input="drugname")
-print(input1)
+#input1 = soup.find(input="drugname")
+#print(input1)
+url = "https://www.fda.gov/downloads/Drugs/DevelopmentApprovalProcess/UCM071118.pdf"
+webFile = urllib.urlopen(url)
+pdfFile = open(url.split('/')[-1], 'w')
+pdfFile.write(webFile.read())
+webFile.close()
+#pdfFile.close()
+
+pdfName = pdfFile.name
+print(pdfName)
+fixPDF(pdfName)
+
+#with open(pdfName, "rb") as infile:
+    #source : https://www.reddit.com/r/Python/comments/8pepw9/im_struggling_to_read_text_from_a_pdf_using_the/
+   # input1 = PyPDF2.PdfFileReader(infile)
+   # print("document1.pdf has %d pages." % input1.getNumPages())
+
+
+pdfOpen = open('zq008603_Coursework_1_24008603.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(pdfOpen)
+print(pdfReader.numPages)
+
+for i in range(pdfReader.numPages):
+    page = pdfReader.getPage(i)
+    print('Page no - ' + str(1+pdfReader.getPageNumber(page)))
+    content = page.extractText()
+   # print(content)
+
