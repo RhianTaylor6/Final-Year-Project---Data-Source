7551862909f5b375c97ea0f23b08572cc347e35f
Rhian Taylor <rhiant413@gmail.com>
2018-11-21 09:47:03 +0000

    can find the FDA file and import to the project and unzip

--- findFile.py
+++ findFile.py
@@ -1,18 +1,41 @@
 import importlib
 import urllib
 from bs4 import BeautifulSoup
+from lxml import etree
+import pandas as pd
+import setuptools
+import re
+import numpy as np
+from subprocess import call
+import pytz #timezones
 
-fdaSite = 'https://www.fda.gov/Drugs/InformationOnDrugs/ucm129662.htm'
-#the file is under additional resources, Orange Book Data files
-page_html = urllib.urlopen(fdaSite)
-soup = BeautifulSoup(page_html, 'html.parser')
 
-#for link in soup.find_all('a'):
-    #print(link.get('href'))
+def get_link_from_fda():
+    fdaSite = 'https://www.fda.gov/Drugs/InformationOnDrugs/ucm129662.htm'
+    #the file is under additional resources, Orange Book Data files
+    page_html = urllib.request.urlopen(fdaSite) #needed to add the .request. to make it work for python3
+    soup = BeautifulSoup(page_html, 'html.parser')
 
-for i in soup.find_all('li'):
-    #need an if li contains Orange Book Data files then print i
-    print(i)
+    soupStr = soup.find_all('a')
+
+    for link in soupStr:
+        #print(str(link.get('href')))
+        if "Orange Book Data Files (compressed)" in str(link): #checking each a tag for the target file
+            return str('https://www.fda.gov' + str(link).split('"')[1])
+
+urllib.request.urlretrieve(get_link_from_fda(),"tmpfda.zip")# retrieveing the file, 
+call(['rm','-r','tmpfda*']) #removing any files starting with with tmpfda so we don't hav multipl copies
+call(['unzip', 'tmpfda.zip', '-d', 'tmpfdazip'])#unzipping the new copy
+
+
+
+
+
+
+
+
+        
+  
 
 
 
