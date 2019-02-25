2ada0f0cd37f222ecf35be6098915f4a6daacaa3
Rhian Taylor <rhiant413@gmail.com>
2019-02-25 13:32:43 +0000

    Flask Register
    Pulls info succesfully, next step database

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ def serve_webpage(path):
     return flask.send_from_directory('./web_contents/', path)
 
 
-@app.route("/<path:path>", methods=["POST"])
-def register_post(path):
+@app.route("/register", methods=["POST"])
+def register_post():
     username = flask.request.form['username']
     password = flask.request.form['password']
     c_password = flask.request.form['c-password']
     email = flask.request.form['email']
     company = flask.request.form['company']
 
+    print(username + password + c_password + email + company)
     return '{},{},{},{}'.format(username, password, c_password, email, company)
 
 
-app.run(debug=True)
+app.run(debug=True, port=80)
