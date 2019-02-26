4a02c6b08e8dcd81bd16de5dc3b022afcb12e824
Rhian Taylor <rhiant413@gmail.com>
2019-02-26 17:32:19 +0000

    Registration form validation
    Check that passwords match and put validation on the other fields

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flask
 
 app = flask.Flask(__name__, static_url_path='/web_contents/')
+
+app.config['SECRET KEY'] = '2f76c03428808188004157ebf4932088'
+
 @app.route("/<path:path>")
 def serve_webpage(path):
     return flask.send_from_directory('./web_contents/', path)
@@ -8,14 +11,18 @@ def serve_webpage(path):
 
 @app.route("/register", methods=["POST"])
 def register_post():
-    username = flask.request.form['username']
+    f_name = flask.request.form['f_name']
+    l_name = flask.request.form['l_name']
     password = flask.request.form['password']
     c_password = flask.request.form['c-password']
     email = flask.request.form['email']
     company = flask.request.form['company']
 
-    print(username + password + c_password + email + company)
-    return '{},{},{},{}'.format(username, password, c_password, email, company)
+    
+    return '{},{},{},{},{},{}'.format(f_name, l_name, password, c_password, email, company)
 
+@app.route("/login", methods=["GET","POST"])
+def login():
+    return print(login)
 
 app.run(debug=True, port=80)
--- readFile.py
+++ readFile.py
@@ -1,15 +1,23 @@
 import pandas as pd
 
 drugsFDAdf = pd.read_csv("tmpfdazip/products.txt", delimiter='~')
-
+patentFDAdf = pd.read_csv("tmpfdazip/patent.txt", delimiter='~')
+exclusivityFDAdf = pd.read_csv("tmpfdazip/exclusivity.txt", delimiter='~')
 
 # print(drugsFDAdf.head)
 print(list(drugsFDAdf))
+print(list(patentFDAdf))
+print(list(exclusivityFDAdf))
 
-drugsFDAdfCleaned = drugsFDAdf[['Trade_Name', 'Applicant', 'Approval_Date']]
+patentFDAdfCleaned = patentFDAdf[['Appl_No', 'Product_No', 'Patent_No', 'Submission_Date']]
+exclusivityFDAdfCleaned = exclusivityFDAdf[['Appl_No', 'Exclusivity_Code','Exclusivity_Date']]
+drugsFDAdfCleaned = drugsFDAdf[['Appl_No', 'Trade_Name', 'Ingredient', 'Applicant', 'Approval_Date', 'Type']]
+
+FDAdf = pd.merge(patentFDAdfCleaned, exclusivityFDAdfCleaned, on='Appl_No')
+FDAdf = pd.merge(drugsFDAdfCleaned, FDAdf,  on='Appl_No')
 #drugsFDAdfCleaned = drugsFDAdfCleaned.drop_duplicates()
 #drugsFDAdfCleaned = drugsFDAdfCleaned.groupby('Applicant')
-
-print(drugsFDAdfCleaned)
+print(FDAdf)
+#print(drugsFDAdfCleaned)
 # for key, items in drugsFDAdfCleaned:
 # print(drugsFDAdfCleaned.get_group(key))
