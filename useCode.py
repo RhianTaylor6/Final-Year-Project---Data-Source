e45f523815c0cbdc6babef1e17bd0c9ed068d60a
Rhian Taylor <rhiant413@gmail.com>
2019-03-04 16:40:30 +0000

    Uses
    Pulls uses successfully from list of URLs generated

--- useCode.py
+++ useCode.py
@@ -16,24 +16,30 @@ def createURL(df):
         appType = row['Appl_Type']
         constructURL = 'https://www.accessdata.fda.gov/scripts/cder/ob/patent_info.cfm?Product_No={}&Appl_No={}&Appl_type={}'
         constructedURL.append(constructURL.format(prodNo,appNo,appType))
-        print(constructedURL[index])
+        #print(constructedURL[index])
     return constructedURL
 
 
-"""
-page_html = urllib.request.urlopen()
+def getUse(urlList):
+    for i in range(len(urlList)):
+        page_html = urllib.request.urlopen(urlList[i])
 
-soup = BeautifulSoup(page_html,'html.parser')
+        soup = BeautifulSoup(page_html,'html.parser')
 
-for x in soup.find_all():
-    if len(x.text) == 0:
-        x.extract()
+        for x in soup.find_all():
+            if len(x.text) == 0:
+                x.extract()
 
-use = soup.find_all(class_="tooltiptext")
-useCodes= soup.find_all(text=re.compile('U-'))
-numUseCodes= len(soup.find_all(text=re.compile('U-')))
+        use = soup.find_all(class_="tooltiptext")
+        useCodes= soup.find_all(text=re.compile('U-'))
+        numUseCodes= len(soup.find_all(text=re.compile('U-')))
 
-for i in range(numUseCodes):
-    print(useCodes[i]+ " : " + use[i].text)
-"""
-createURL(readFile.useSearch())
+        for i in range(numUseCodes):
+            print(useCodes[i]+ " : " + use[i].text)
+            print("\n")
+        
+        print("New Drug:\n" )
+
+url_list = createURL(readFile.useSearch())
+
+getUse(url_list)
