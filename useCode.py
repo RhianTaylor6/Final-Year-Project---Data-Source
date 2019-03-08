cf46126221d610498695b008307d530a029220fa
Rhian Taylor <rhiant413@gmail.com>
2019-03-08 16:12:26 +0000

    Uses csv
    saves all uses and codes to CSV

--- useCode.py
+++ useCode.py
@@ -6,6 +6,7 @@ import readFile
 import re
 import pandas as pd
 import numpy as np
+import csv
 
 
 def createURL(df):
@@ -21,25 +22,39 @@ def createURL(df):
 
 
 def getUse(urlList):
-    for i in range(len(urlList)):
-        page_html = urllib.request.urlopen(urlList[i])
 
-        soup = BeautifulSoup(page_html,'html.parser')
+    with open('uses.csv', mode = 'w') as use_file:
+        use_writer = csv.writer(use_file, delimiter=',')
+        use_writer.writerow(['URL',' Use Code','Use'])
+        for urls in range(len(urlList)):
+            #print("\n"+urlList[urls]+"\n")
+            page_html = urllib.request.urlopen(urlList[urls])
 
-        for x in soup.find_all():
-            if len(x.text) == 0:
-                x.extract()
+            soup = BeautifulSoup(page_html,'html.parser')
 
-        use = soup.find_all(class_="tooltiptext")
-        useCodes= soup.find_all(text=re.compile('U-'))
-        numUseCodes= len(soup.find_all(text=re.compile('U-')))
+            for x in soup.find_all():
+                if len(x.text) == 0:
+                    x.extract()
 
-        for i in range(numUseCodes):
-            print(useCodes[i]+ " : " + use[i].text)
-            print("\n")
+            use = soup.find_all(class_="tooltiptext")
+            useCodes= soup.find_all(text=re.compile('U-'))
+            numUseCodes= len(soup.find_all(text=re.compile('U-')))
+
+            for codes in range(numUseCodes):
+                #print(useCodes[codes]+ " : " + use[codes].text)
+                use_writer.writerow([urlList[urls],useCodes[codes],use[codes].text])
+            
+    
+    
+
+
+
+    
         
-        print("New Drug:\n" )
 
 url_list = createURL(readFile.useSearch())
 
-getUse(url_list)
+getUse( url_list)
+df = readFile.useSearch()
+df['URL']=url_list
+
