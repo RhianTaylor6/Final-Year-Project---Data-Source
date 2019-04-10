6ef500059fa34e40b8c4a9714d8c50e37c972e52
Rhian Taylor <rhiant413@gmail.com>
2019-04-10 12:45:40 +0100

    Fuzzy Logic
    Completed and written to csv

--- FDA_Module/use_list_generator.py
+++ FDA_Module/use_list_generator.py
@@ -21,11 +21,11 @@ drugs = pd.read_csv("FDA_Module/therapydrugs.csv", dtype=dtype_dic)
 therapy_area = pd.read_csv("FDA_Module/therapyurls.csv", dtype=dtype_dic)
 
 def write_use_list(df):
-    with open('FDA_Module/'+df.name+'.csv', mode='w') as FDA_file:
+    with open('FDA_Module/therapeuticGroups.csv', mode='w') as FDA_file:
         use_writer = csv.writer(FDA_file, delimiter=',')
-        use_writer.writerow([ ' Use Code', 'Use'])
+        use_writer.writerow([ ' Use Code', 'Use', 'Therapeutic_Area', 'Use_Treatment_WRatio'])
         for index, row in df.iterrows():
-            use_writer.writerow([row[' Use Code'], row['Use']])
+            use_writer.writerow([row[' Use Code'], row['Use'],row['Therapeutic_Area'], row['Use_Treatment_WRatio']])
 
 
 use_codes = fdadf[[' Use Code', 'Use']]
@@ -42,25 +42,27 @@ therapyDrugs = pd.merge(drugs,therapy_area, on = ['URL'])
 therapyDrugs["treatment"].replace(r'\s+|\\n', ' ', regex=True, inplace=True) 
 therapyDrugs["treatment"] = therapyDrugs["treatment"].str.upper()
 
+
 treatments = therapyDrugs[['treatment','Therapy_area']]
+highest_ratio = 0
+use_therapy = pd.DataFrame({" Use Code":[], "Therapeutic_Area":[],"Use_Treatment_WRatio":[]})   
 
-trying = therapyDrugs["treatment"].str.contains("psoriasis")
-
-ratio = 0
-
-use_therapy = []
-
-
-for index, row in treatments.iterrows():
-    #ratio = fuzz.WRatio(row[use_codes["Use"]], row[treatments["treatment"]])
-    treatmentSTR = row.treatment
-    #print(row.treatment)
-    for index,row in use_codes.iterrows():
-        #ratio = fuzz.WRatio(use_codes["Use"][use], treatments["treatment"][treatment])
-        #print(row.Use)
-        useSTR = row.Use
-        ratio = fuzz.WRatio(treatmentSTR, useSTR)
-        print(ratio)
+with open('FDA_Module/therapeuticGroups.csv', mode='w') as FDA_file:
+    use_writer = csv.writer(FDA_file, delimiter=',')
+    use_writer.writerow([ ' Use Code', 'Use', 'Therapeutic_Area', 'Use_Treatment_WRatio'])   
+    for indexA, row in treatments.iterrows():
+        treatmentSTR = row.treatment
+        for indexB,row in use_codes.iterrows():
+            useSTR = row.Use
+            ratio = fuzz.WRatio(treatmentSTR, useSTR)
+            if ratio >= highest_ratio:
+                highest_ratio = ratio
+                use_writer.writerow([use_codes[' Use Code'][indexB], use_codes['Use'][indexB],treatments['Therapy_area'][indexA], str(highest_ratio)])
+                use_therapy.append({" Use Code":[use_codes[' Use Code'][indexB]],"Therapeutic_Area":[treatments['Therapy_area'][indexA]], "Use_Treatment_WRatio": [str(highest_ratio)]},ignore_index=True)
+                print([use_codes[' Use Code'][indexB], use_codes['Use'][indexB],treatments['Therapy_area'][indexA], str(highest_ratio)])
+                #print([use_codes[' Use Code'][indexB], use_codes['Use'][indexB],treatments['Therapy_area'][indexA], str(highest_ratio)])
+#use_codes = pd.merge(use_codes,use_therapy, on = [' Use Code'])
+#write_use_list(use_codes)
 
 
 
